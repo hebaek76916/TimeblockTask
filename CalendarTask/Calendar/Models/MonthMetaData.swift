8fa0638eb576c13dd07796bd964c5c78d8ea12d8
hebaek76916 <qorgusdms15@gmail.com>
2022-02-17 23:00:04 +0900

    set 'MonthMetaData' struct

---
+++ CalendarTask/Calendar/Models/MonthMetaData.swift
@@ -0,0 +1,14 @@
+//
+//  MonthMetaData.swift
+//  CalendarTask
+//
+//  Created by 현은백 on 2022/02/15.
+//
+
+import Foundation
+
+struct MonthMetaData {
+    let numberOfDays: Int
+    let firstDay: Date
+    let firstDayWeekday: Int
+}
