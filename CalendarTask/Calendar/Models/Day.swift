2a3b2f38869c8490bd94d03c07a7b1762a48e174
hebaek76916 <qorgusdms15@gmail.com>
2022-02-17 22:59:12 +0900

    set 'Day' struct

---
+++ CalendarTask/Calendar/Models/Day.swift
@@ -0,0 +1,15 @@
+//
+//  Day.swift
+//  CalendarTask
+//
+//  Created by 현은백 on 2022/02/15.
+//
+
+import Foundation
+
+struct Day {
+  let date: Date
+  let number: String
+  let isSelected: Bool
+  let isWithinDisplayedMonth: Bool
+}
