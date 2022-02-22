3949f5edeeaf2f12f0c741dc0618f1f4f228bb17
hebaek76916 <qorgusdms15@gmail.com>
2022-02-22 11:22:28 +0900

    Add UIColor(rnd Color), UIFont(unify Font) Extension.

--- CalendarTask/Extensions.swift
+++ CalendarTask/Extensions.swift
@@ -20,5 +20,31 @@ extension UIView {
         let randomBlue:CGFloat = CGFloat(drand48())
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
+}
 
+extension UIColor {
+    static func random() -> UIColor {
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+}
+
+extension UIFont {
+    enum Bold {
+        case _14
+        case _10
+        
+        var font: UIFont {
+            switch self {
+            case ._10: return UIFont.boldSystemFont(ofSize: 10)
+            case ._14: return UIFont.boldSystemFont(ofSize: 10)
+            }
+        }
+    }
+    
+    enum Regular {
+        
+    }
 }
